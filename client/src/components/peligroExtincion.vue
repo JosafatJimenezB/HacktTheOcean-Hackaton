b163d99a264e1a841e8853e8a48e52332c706940
JosafatJimenezB <josa221001@gmail.com>
2022-05-14 16:49:55 -0500

    add: resolviendo bugs

--- client/src/components/peligroExtincion.vue
+++ client/src/components/peligroExtincion.vue
@@ -61,4 +61,17 @@
 </style>
 
 
+<script>
+    import Swiper from 'swiper';
+
+
+    var swiper = new Swiper(".mySwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+    });
+</script>
+
+
 
