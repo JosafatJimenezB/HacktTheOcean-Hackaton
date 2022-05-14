efe9c077d556a1e3fd681f2d4c85f31dc99d9bb4
JosafatJimenezB <josa221001@gmail.com>
2022-05-14 17:21:18 -0500

    add: añadiendo archivos y removiendo swiperjs

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="navbar">
         <div class="logo">
-            <h2>Conservacion marina</h2>
         </div>
         <nav>
             <router-link class="nav-item" to="/">Home</router-link>
--- client/src/components/peligroExtincion.vue
+++ client/src/components/peligroExtincion.vue
@@ -8,21 +8,7 @@
                     Hechos tales como la pesca incontrolada, la acidificación del agua, la gran contaminación que sufren los mares y océanos, y el cambio climático, del cual, somos los principales responsables, está teniendo como consecuencia que numerosas especies marinas se encuentren en peligro de extinción.</p>
             </div>
             <div class="carrusel">
-                <div class="swiper mySwiper">
-                    <div class="swiper-wrapper">
-                        <div class="swiper-slide">Slide 1</div>
-                        <div class="swiper-slide">Slide 2</div>
-                        <div class="swiper-slide">Slide 3</div>
-                        <div class="swiper-slide">Slide 4</div>
-                        <div class="swiper-slide">Slide 5</div>
-                        <div class="swiper-slide">Slide 6</div>
-                        <div class="swiper-slide">Slide 7</div>
-                        <div class="swiper-slide">Slide 8</div>
-                        <div class="swiper-slide">Slide 9</div>
-                    </div>
-                    <div class="swiper-button-next"></div>
-                    <div class="swiper-button-prev"></div>
-                </div>
+               <h1>hola</h1>
             </div>
         </div>
     </div>
@@ -62,15 +48,7 @@
 
 
 <script>
-    import Swiper from 'swiper';
-
-
-    var swiper = new Swiper(".mySwiper", {
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-    });
+ 
 </script>
 
 
